663ccc9715504cdb7de1cd7b5ea82151a3b4a6a4
zhangshiqi <q18056090106@163.com>
2022-07-15 08:28:41 +0800

    feat: ✨ 分页组件基本功能实现

--- src/components/pagination/src/BPagination.vue
+++ src/components/pagination/src/BPagination.vue
@@ -1,45 +1,49 @@
 <template>
   <div class="b-pag-box">
-    <!-- 是否禁用 判断当前页码是否是 等于 1 -->
+    <!-- 上一页 -->
     <a
       class="b-right-arrow b-outside-border"
-      :class="{ disabled: isCurrentPage <= 1 }"
-      @click="changePage(isCurrentPage - 1)"
+      :class="{ disabled: currentPageNum <= 1 }"
+      @click="changePage(false)"
       href="javascript:;"
     >
       &lt;
     </a>
-    <!-- 是否显示 判断起始页码是否 大于 1 -->
-    <span class="b-page-size b-outside-border" v-if="pager.start > 1">...</span>
+
+    <!-- ... -->
+    <!-- <a class="b-page-size b-outside-border" v-if="pager.start > 1" href="javascript:;">...</a> -->
+
+    <!-- 页数按钮 -->
     <a
       class="b-page-size b-outside-border"
       v-for="i in pager.btnArr"
       :key="i"
       href="javascript:;"
-      :class="{ active: i === isCurrentPage }"
+      :class="{ active: i === currentPageNum }"
       @click="changePage(i)"
       >{{ i }}</a
     >
-    <!-- 是否显示 判断 结束页码 是否 小于总页数 -->
-    <span
-      class="b-page-size b-outside-border"
-      v-if="pager.end < pager.pageCount"
-      >...</span
-    >
+
+    <!-- ... -->
+    <!-- <a class="b-page-size b-outside-border" v-if="pager.end < pages" href="javascript:;">...</a> -->
+
+    <!-- 下一页 -->
     <a
       class="b-right-arrow b-outside-border"
-      :class="{ disabled: isCurrentPage >= pager.pageCount }"
+      :class="{ disabled: currentPageNum >= pages }"
       href="javascript:;"
-      @click="changePage(isCurrentPage + 1)"
+      @click="changePage(true)"
     >
       >
     </a>
+
+    <!-- 选择一页条数 -->
     <div
       class="b-page-num b-num-out-size-border"
       @click="openPageNum = !openPageNum"
     >
       <div class="b-text-page-num">
-        <span>10</span>
+        <span>{{ pageSize }}</span>
         <span>条/页</span>
       </div>
       <img
@@ -54,9 +58,9 @@
       <div class="b-select-dropdown b-popper" v-show="openPageNum">
         <div
           class="b-select-dropdown-item"
-          v-for="(item, index) in [10, 20, 30]"
+          v-for="item in pageNum"
           :key="item"
-          @click="changePages(index)"
+          @click="changePages(item)"
         >
           <span>{{ item }}</span>
           <span>条/页</span>
@@ -66,99 +70,152 @@
   </div>
 </template>
 
-<script setup lang="ts">
-import { toRefs, watch, reactive, computed, withDefaults } from 'vue'
-
-// 导入接口
-import { pageType, pageDataType } from './interface'
-
-const prors = withDefaults(defineProps<pageType>(), {
-  total: 100,
-  currentPage: 1,
-  pageSize: 10
-})
-
-// 响应式数据绑定
-const pageData = reactive<pageDataType>({
-  // 总条数
-  isTotal: 100,
-  // 当前页码
-  isCurrentPage: 1,
-  // 每页几条数据
-  isPageSize: 10,
-  // 按钮的个数
-  btnCount: 5,
-  // 打开页码列表
-  openPageNum: false
-})
-const { isTotal, isCurrentPage, isPageSize, btnCount, openPageNum } =
-  toRefs(pageData)
-// 监听传入的数据
-watch(prors, () => {
-  isTotal.value = prors.total
-  isCurrentPage.value = prors.currentPage
-  isPageSize.value = prors.pageSize
-})
-
-// * 根据上述数据得到（总页数，起始页码，结束页码，按钮数组）
-const pager = computed(() => {
-  // 计算总页数
-  const pageCount = Math.ceil(isTotal.value / isPageSize.value)
-  // 计算起始页码 和 结束页码
-  // 1 - 理想情况 - 根据当前页码个数可得到
-  let start = isCurrentPage.value - Math.floor(btnCount.value / 2)
-  let end = start + btnCount.value - 1
-  // 2.1 - 如果起始页码小于1，需要重新计算
-  if (start < 1) {
-    start = 1
-    end =
-      start + btnCount.value - 1 > pageCount
-        ? pageCount
-        : start + btnCount.value - 1
-  }
-  // 2.2 - 如果结束页码大于总页数，需要重新计算
-  if (end > pageCount) {
-    end = pageCount
-    start = end - btnCount.value + 1 < 1 ? 1 : end - btnCount.value + 1
-  }
-
-  // 处理完毕 start 和 end 得到按钮数组
-  const btnArr = []
-  for (let i = start; i <= end; i++) {
-    btnArr.push(i)
-  }
-  return {
-    pageCount,
-    start,
-    end,
-    btnArr
-  }
-})
-
-const changePages = (index: number) => {
-  console.log(index)
-}
-
-// 定义一个自定义事件 向父传参
-const emit = defineEmits(['current-change'])
-
-// 改变页码
-const changePage = (newPage: number) => {
-  isCurrentPage.value = newPage
-  if (isCurrentPage.value < 1) {
-    isCurrentPage.value = 1
-  } else if (isCurrentPage.value > pager.value.pageCount) {
-    isCurrentPage.value = pager.value.pageCount
-  }
-  // 通知父组件最新页码
-  emit('current-change', newPage)
-}
-</script>
-
 <script lang="ts">
-export default {
-  name: 'BPagination'
-}
+import { toRefs, ref, defineComponent, computed } from 'vue'
+import type { PropType } from 'vue'
+// 导入接口
+export default defineComponent({
+  name: 'BPagination',
+  props: {
+    // 总条数
+    total: {
+      type: Number as PropType<number>,
+      default: 100
+    },
+    // // 当前页
+    currentPage: {
+      type: Number as PropType<number>,
+      default: 1
+    },
+    // 一页的数量
+    pageSize: {
+      type: Number as PropType<number>,
+      default: 10
+    }
+  },
+  emits: ['change-page', 'page-size-change'],
+  setup(props, { emit }) {
+    const { total, pageSize } = toRefs(props)
+    // 下拉页数
+    const openPageNum = ref(false)
+    // 每页条数的选择
+    const pageNum = [10, 20, 30]
+    // 按钮的个数
+    const btnCount = 10
+    // 总页数
+    const pages = computed(() => Math.ceil(total.value / pageSize.value))
+
+    // 当前页码
+    const currentPageNum = ref(props.currentPage || 1)
+
+    // 动态计算页码列表
+    const list = computed(() => {
+      // 当父组件传递total的值发生变化时，计算属性会重新计算
+      // pages = Math.ceil(props.total / props.pagesize)
+      const result = []
+      // 总页码小于等于5；大于5
+      if (pages.value <= 5) {
+        // 总页码小于等于5的情况
+        for (let i = 1; i <= pages.value; i++) {
+          result.push(i)
+        }
+      } else {
+        // 总页码大于5
+        if (currentPageNum.value <= 2) {
+          // 左侧临界值
+          for (let i = 1; i <= 5; i++) {
+            result.push(i)
+          }
+        } else if (currentPageNum.value >= currentPageNum.value - 1) {
+          // 右侧临界值
+          for (let i = pages.value - 4; i <= pages.value; i++) {
+            result.push(i)
+          }
+        } else {
+          // 中间的状态
+          for (
+            let i = currentPageNum.value - 2;
+            i <= currentPageNum.value + 2;
+            i++
+          ) {
+            result.push(i)
+          }
+        }
+      }
+      return result
+    })
+
+    // * 根据上述数据得到（总页数，起始页码，结束页码，按钮数组）
+    const pager = computed(() => {
+      // 计算起始页码 和 结束页码
+      let start = currentPageNum.value - Math.floor(btnCount / 2)
+      let end = start + btnCount - 1
+      // 2.1 - 如果起始页码小于1，需要重新计算
+      if (start < 1) {
+        start = 1
+        end =
+          start + btnCount - 1 > pages.value
+            ? pages.value
+            : start + btnCount - 1
+      }
+      // 2.2 - 如果结束页码大于总页数，需要重新计算
+      if (end > pages.value) {
+        end = pages.value
+        start = end - btnCount + 1 < 1 ? 1 : end - btnCount + 1
+      }
+      // 处理完毕 start 和 end 得到按钮数组
+      const btnArr = []
+      for (let i = start; i <= end; i++) {
+        btnArr.push(i)
+      }
+      return {
+        start,
+        end,
+        btnArr
+      }
+    })
+
+    // 控制上一页和下一页变化
+    const changePage = (type: number | boolean) => {
+      if (type === false) {
+        // 上一页
+        // 页面是第一页时，禁止点击操作
+        if (currentPageNum.value === 1) return
+        if (currentPageNum.value > 1) {
+          currentPageNum.value -= 1
+        }
+      } else if (type === true) {
+        // 下一页
+        // 页面是最后页时，禁止点击操作
+        if (currentPageNum.value === pages.value) return
+        if (currentPageNum.value < pages.value) {
+          currentPageNum.value += 1
+        }
+      } else {
+        // 点击页码
+        currentPageNum.value = type
+      }
+      emit('change-page', currentPageNum.value)
+    }
+
+    // 选择每页显示的条数
+    function changePages(num: number) {
+      if (pageSize.value === num) return
+      currentPageNum.value = 1
+      emit('page-size-change', num)
+    }
+    return {
+      list,
+      currentPageNum,
+      pageNum,
+      pages,
+      openPageNum,
+      pager,
+      changePage,
+      changePages
+    }
+  }
+})
 </script>
 
 <style scoped lang="less">
--- src/views/paginationDemo.vue
+++ src/views/paginationDemo.vue
@@ -4,16 +4,40 @@
       :total="total"
       :pageSize="pageSize"
       :currentPage="currentPage"
+      @change-page="changepage"
+      @page-size-change="pageSizeChange"
     />
   </div>
 </template>
 
-<script setup lang="ts">
-import BPagination from '@/components/pagination'
-import { ref } from 'vue'
-const total = ref(1)
-const pageSize = ref(10)
-const currentPage = ref(1)
+<script lang="ts">
+import { toRefs, defineComponent, reactive } from 'vue'
+import { pageType } from '@/components/pagination/src/interface'
+import BPagination from '@/components/pagination/src/BPagination.vue'
+export default defineComponent({
+  setup() {
+    const data = reactive<pageType>({
+      total: 100,
+      pageSize: 10,
+      currentPage: 1
+    })
+    const { total, pageSize, currentPage } = toRefs(data)
+    function changepage(value: number) {
+      currentPage.value = value
+    }
+    function pageSizeChange(val: number) {
+      pageSize.value = val
+    }
+    return {
+      total,
+      pageSize,
+      currentPage,
+      changepage,
+      pageSizeChange
+    }
+  },
+  components: { BPagination }
+})
 </script>
 
 <style scoped lang="less">
