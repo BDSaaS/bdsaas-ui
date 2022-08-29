e33075a02de1b2c2591b8e6f9691a4480ba60989
fengchao <1527218391@qq.com>
2022-08-29 14:19:20 +0800

    icon

--- new-examples/src/main.ts
+++ new-examples/src/main.ts
@@ -3,6 +3,6 @@ import App from './App.vue'
 import router from '@/router'
 import BdUI from 'bdsaas-ui'
 import 'bdsaas-ui/src/style/index.less'
-import './assets/fonts/index.css'
+import './assets/fonts/style.css'
 
 createApp(App).use(router).use(BdUI).mount('#app')
