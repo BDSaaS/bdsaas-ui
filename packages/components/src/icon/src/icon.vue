13bb336ac198aa25d1809d255261cd9e9cbfd7d1
fengchao <1527218391@qq.com>
2022-08-31 09:39:30 +0800

    icon

--- packages/components/src/icon/src/icon.vue
+++ packages/components/src/icon/src/icon.vue
@@ -1,5 +1,5 @@
 <template>
-  <span class="b-icon" :class="['b-icon-' + name]"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span></span>
+  <span class="b-icon" :class="['b-icon-' + name]"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span></span>
 </template>
 
 <script lang="ts">
